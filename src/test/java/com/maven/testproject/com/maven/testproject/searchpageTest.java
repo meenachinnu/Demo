3318307f0dfe12ecbf5ec475c10bff67b0c73e5d
meena <chinnumohu@gmail.com>
2019-02-22 15:23:36 +0530

    added in one file

---
+++ src/test/java/com/maven/testproject/com/maven/testproject/searchpageTest.java
@@ -0,0 +1,13 @@
+package com.maven.testproject.com.maven.testproject;
+
+import org.testng.annotations.Test;
+
+public class searchpageTest {
+
+	
+	@Test
+	public void test(){
+		
+		System.out.println("sample");
+	}
+}
